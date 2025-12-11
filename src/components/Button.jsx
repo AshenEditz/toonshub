cd1191814b258ee8a23c446b6fc9e6bd16c6386a
AshenEditZ <officialcyber97@outlook.com>
2025-12-11 23:32:40 +0530

    Add src/components/Button.jsx

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Button({ children, onClick, variant = 'primary', ...props }) {
+  return (
+    <button 
+      className={`btn btn-${variant}`}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
